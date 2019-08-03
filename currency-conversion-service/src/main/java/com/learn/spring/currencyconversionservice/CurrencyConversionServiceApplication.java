c033e2d20031dec2c46f1d9f715742027bd27bde
aneeshkapur1 <aneesh.kapur18@gmail.com>
2019-08-03 17:35:50 +0530

    changes to services

--- currency-conversion-service/src/main/java/com/learn/spring/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/learn/spring/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,12 @@ package com.learn.spring.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.learn.spring.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
--- currency-conversion-service/src/main/java/com/learn/spring/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/learn/spring/currencyconversionservice/controller/CurrencyConversionController.java
@@ -2,6 +2,9 @@ package com.learn.spring.currencyconversionservice.controller;
 
 
 import com.learn.spring.currencyconversionservice.model.CurrencyConversionBean;
+import com.learn.spring.currencyconversionservice.service.CurrencyExchangeServiceProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -15,7 +18,10 @@ import java.util.Map;
 public class CurrencyConversionController {
 
 
+    @Autowired
+    CurrencyExchangeServiceProxy proxy;
 
+/*
     @RequestMapping(path = "/currency-converter/from/{from}/to/{to}/quantity/{quantity}",method = RequestMethod.GET)
     public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity)
     {
@@ -30,6 +36,19 @@ public class CurrencyConversionController {
         CurrencyConversionBean response = responseEntity.getBody();
 
         return new CurrencyConversionBean(response.getId(),from,to,response.getConversionMultiple(),quantity,quantity.multiply(response.getConversionMultiple()),0);
+
+    }
+*/
+
+    @RequestMapping(path = "/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}",method = RequestMethod.GET)
+    public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity)
+    {
+
+
+
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from,to);
+
+        return new CurrencyConversionBean(response.getId(),from,to,response.getConversionMultiple(),quantity,quantity.multiply(response.getConversionMultiple()),response.getPort());
 
     }
 
